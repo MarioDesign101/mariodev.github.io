ea1d7d73725d76387480a0ba035fdd4fab479454
Mario Deisgn - Desarrollador y diseñador web <141463275+MarioDesign101@users.noreply.github.com>
2025-03-07 10:18:08 -0600

    Delete config directory

--- config/js/scroll.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('.header__general');
-    if(window.scrollY > 400) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
-})
